11ab944d4115bf4cfb30e4b07dcf02b833f6e629
rodrigoayacura <rodrigo.ayacura@inacapmail.cl>
2018-07-04 23:22:38 -0400

    ParaInicio
    Se crea archivo index copia

---
+++ index - copia.php
@@ -0,0 +1,104 @@
+  <?php
+    include ("assets/incrustado/sesion.php");
+       ?>
+  <!doctype html>
+  <html lang="es-CL">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport"
+            content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>My Baby-sitter</title>
+  	<?php
+  	    include( "assets/incrustado/head.php" );
+  	?>
+  </head>
+  <body style="background-color:#f5f5f5!important;">
+  	<section class="navegacion">
+          <header class="cabecera">
+              <?php
+                  include("assets/incrustado/navbar.php")
+              ?>
+          </header>
+  <div name="Contenido">
+
+  <div name="ComoFunciona">
+
+  </div>
+
+  <div name="QuienesSomos">
+    </div>
+
+  <div name="txtComoFunciona">
+    <p>La Guardería es una plataforma que conecta de manera confiable, rápida y segura a padres con babysitters sicológicamente aprobadas y calificadas por nuestro equipo.</p>
+                          <p>Para ser parte de nuestra comunidad solo sigue los siguientes pasos:</p>
+
+  <div name="CuadroIzq">
+<div class="col-xs-12 col-sm-4 col-sm-offset-1 text-center">
+                          <img src="https://www.laguarderia.cl/wp-content/themes/la-guarderia/assets/img/tit-inscripcion.png" alt="">
+                          <h3>¿Buscas <span>babysitter?</span></h3>
+
+                          <div class="space20"></div>
+
+                          <div class="row">
+                              <div class="col-xs-12 col-sm-10 col-sm-offset-1">
+                                  <img src="https://www.laguarderia.cl/wp-content/themes/la-guarderia/demo/como-funciona-1.jpg" style="max-width:100%;">
+                              </div>
+                          </div>
+                      </div>
+
+
+    </div>
+    
+  <div name="cuadroDer">
+
+                      <div class="col-xs-12 col-sm-4 text-center">
+                          <div class="space40 visible-xs"></div>
+                          <img src="https://www.laguarderia.cl/wp-content/themes/la-guarderia/assets/img/tit-inscripcion.png" alt="">
+                          <h3>¿Quieres <span>Cuidar?</span></h3>
+
+                          <div class="space20"></div>
+
+                          <div class="row">
+                              <div class="col-xs-12">
+                                  <img src="https://www.laguarderia.cl/wp-content/themes/la-guarderia/demo/como-funciona-2.jpg" style="max-width:100%;">
+                              </div>
+                          </div>
+                          
+                      </div>
+
+  </div>
+
+
+
+
+  </div>
+
+  <div name="txtQuienesSomos">
+    <p name="filosofia">
+    “Somos un equipo de mamás, mujeres y amigas que llevamos dedicadas más de 10 años al cuidado de niños, y que sabemos lo importante y satisfactorio que es contar con personas de confianza y responsables para el cuidado de nuestros niños.</p>
+    <br><br>
+  <p>Es por esto, que el año 2011 creamos La Guarderia , una comunidad donde padres, babysitters y niños encuentran lo que necesitan.”
+  </p>
+
+
+  <p name="objetivo">El objetivo principal de La Guardería es generar confianza en el cuidado de niños. Es por esto que hemos creado esta comunidad que permita a los padres encontrar babysitters calificadas de una manera confiable y segura. Todas las babysitters que pertenecen a nuestro buscador han sido evaluadas sicológicamente, y tienen la posibilidad de capacitarse en primeros auxilios.<br><br>
+  Nuestro Staff cuenta con educadoras de párvulo que planifican actividades de capacitación a las babysitters para que puedan complementar su cuidado y que de esta forma los niños puedan desarrollar distintas áreas del aprendizaje según las edades e intereses.<br><br>
+  En La Guardería no sólo nos preocupamos de entregar cuidado, sinó de que también tus niños aprendan y lo pasen bien, recibiendo el cariño y la confianza que nos ha caracterizado siempre, desde que comenzamos el año 2011.</p></div>
+    </div>
+
+
+  </div>
+
+
+                  </div>
+
+              </div>
+
+
+      </section>
+  	<?php
+  	include( "assets/incrustado/pie-js.php" );
+  	?>
+  </body>
+  </html>
